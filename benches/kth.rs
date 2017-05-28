6f25336a06bde22ccc72c40504df739c2c064de0
Cody P Schafer <dev@codyps.com>
2017-05-28 00:58:14 -0400

    benchmarks, and ones that actually finish (these are _slow_ compared to hash-roll)

--- benches/kth.rs
+++ benches/kth.rs
@@ -11,7 +11,7 @@ use kth::quickselect;
 
 use rand::Rng;
 
-const BENCH_LEN: usize = 1024 * 1024 * 4;
+const BENCH_LEN: usize = 1024 * 512;
 
 #[bench]
 fn median_of_medians_big(b: &mut test::Bencher) {
@@ -21,5 +21,16 @@ fn median_of_medians_big(b: &mut test::Bencher) {
         rng.fill_bytes(&mut d);
         let p = rng.gen::<usize>() % d.len();
         quickselect::quickselect(quickselect::median_of_medians, &mut d[..], p)
+    })
+}
+
+#[bench]
+fn repeated_step3_big(b: &mut test::Bencher) {
+    let mut rng = rand::thread_rng();
+    let mut d = vec![0u8; BENCH_LEN];
+    b.iter(|| {
+        rng.fill_bytes(&mut d);
+        let p = rng.gen::<usize>() % d.len();
+        quickselect::quickselect(quickselect::repeated_step3, &mut d[..], p)
     })
 }
